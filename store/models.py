49dabfc6fe5b805cf9b89da9c42e410a86a23732
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-12-29 23:46:07 +0700

    refactor customer Model and add Order and Address model

--- store/models.py
+++ store/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -12,8 +10,48 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_TYPE = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_TYPE, default=MEMBERSHIP_BRONZE
+    )
+
+
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETED = 'C'
+    PAYMENT_FAILED = 'F'
+
+    PAYMENT_STATUS_TYPE = [
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_COMPLETED, 'Complete'),
+        (PAYMENT_FAILED, 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_TYPE, default=PAYMENT_PENDING
+    )
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+    # NOTE : one to one relation
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, primary_key=True
+    )
