e054865d6c53ae920e39c69ecd525921b28fbcad
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-12-29 23:33:26 +0700

    update customer model

--- store/models.py
+++ store/models.py
@@ -9,3 +9,11 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=6)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
